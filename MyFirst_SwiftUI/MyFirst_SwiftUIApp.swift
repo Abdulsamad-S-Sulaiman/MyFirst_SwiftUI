ad1af4660afdcd8ab97170caee3a03ed416ed7c5
Abdulsamad Salam <abdulsamad.s@the-gw.com>
2023-09-26 11:59:15 +0300

    adding project

---
+++ MyFirst_SwiftUI/MyFirst_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirst_SwiftUIApp.swift
+//  MyFirst_SwiftUI
+//
+//  Created by Kurdi on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirst_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
